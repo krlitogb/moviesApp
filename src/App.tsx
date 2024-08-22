294834a97e803734617c943a3d63ee69d97bbfaa
Carlos Gimenez <krlito.gb@gmail.com>
2024-08-21 20:54:07 -0400

    Instalación de React Navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
+import 'react-native-gesture-handler';
 
+
+import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
 import { Text, View, } from 'react-native';
+import { Navigation } from './presentation/navigation/Navigation';
 
 function App(): React.JSX.Element {
 
-  return (
-   <View>
-    <Text>MoviesApp</Text>
-   </View>
-  );
+ return (
+  <NavigationContainer>
+   <Navigation/>
+
+  </NavigationContainer>
+ );
 }
 
 
