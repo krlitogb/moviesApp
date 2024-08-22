a1be8f7e0455413017ee3ac7c49af7ab807a6a57
Carlos Gimenez <krlito.gb@gmail.com>
2024-08-21 20:33:44 -0400

    Commit inicial - App cero

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import { Text, View, } from 'react-native';
+
+function App(): React.JSX.Element {
+
+  return (
+   <View>
+    <Text>MoviesApp</Text>
+   </View>
+  );
+}
+
+
+export default App;
